75b6ac42a7bd9fb32b5f90e8bbd09c123e46b283
JJayeee <jay.hyundong@gmail.com>
2020-03-31 19:09:47 +0900

    mod tokenizer.py tensorflow tokenizer code test

--- doc/현동/self_study/tokenizer.py
+++ doc/현동/self_study/tokenizer.py
@@ -5,13 +5,18 @@ from sklearn.model_selection import train_test_split
 
 dataset_path = '../../../datasets/captions.csv'
 data = np.loadtxt(dataset_path, delimiter='|', dtype=np.str)
-train_captions = data[:20, 2:]
+train_captions = data[1:101, 2:]
 print('------------train_captions-------------')
 print(train_captions[:5, :])
 print()
-# sample_text_1 = ['I am hungry']
+# train_captions = ['I am hungry']
 # sample_text_2 = '<start> I am hungry <end>'
-
+print(train_captions.shape)
+print(train_captions.ndim)
+train_captions = np.squeeze(train_captions, axis=1)
+print(train_captions.shape)
+print(train_captions.ndim)
+print()
 
 # Find the maximum length of any caption in our dataset
 def calc_max_length(tensor):
@@ -21,7 +26,8 @@ def calc_max_length(tensor):
 # Choose the top 5000 words from the vocabulary
 top_k = 5000
 tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=top_k,
-                                                  oov_token="<unk>",
+                                                  oov_token="<unk>", lower=True,
+                                                  split=' ',
                                                   filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
 
 tokenizer.fit_on_texts(train_captions)
@@ -42,9 +48,12 @@ max_length = calc_max_length(train_seqs)
 
 
 # Create training and validation sets using an 80-20 split
-img_name_train, img_name_val, cap_train, cap_val = train_test_split(img_name_vector,
-                                                                    cap_vector,
-                                                                    test_size=0.2,
-                                                                    random_state=0)
+# cap_train, cap_val = train_test_split(
+#                                                                     cap_vector,
+#                                                                     # test_size=0.2,
+#                                                                     random_state=0)
+print(cap_vector[:10])
+# print(len(cap_train), len(cap_val))
+# print()
+# print(cap_train[:5, :])
 
-print(len(img_name_train), len(cap_train), len(img_name_val), len(cap_val))
