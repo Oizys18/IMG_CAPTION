9d5b710d0d4450c95cc8489928bd689919830b92
이수진 <soojin4964@gmail.com>
2020-04-07 16:38:48 +0900

    Mod feature_extraction.py Add ERROR.md
    Former-commit-id: 7cf156991dd6d2e159c1b42db6aa6a59f2839f1e

--- doc/수진/code/feature_extraction.py
+++ doc/수진/code/feature_extraction.py
@@ -3,28 +3,36 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 
-from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
+from ai_sub2.config import config
 
-import os
-import re
+
 import numpy as np
 import os
-import time
-import json
-from glob import glob
-from PIL import Image
-import pickle
 from tqdm import tqdm
 
 
 
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config_gpu = ConfigProto()
+config_gpu.gpu_options.per_process_gpu_memory_fraction = 0.5
+# config.gpu_options.allow_growth = True
+config_gpu.gpu_options.allow_growth = True
+session = InteractiveSession(config=config_gpu)
+# 지금까지 생성된 tensor 그래프를 제거
+tf.compat.v1.reset_default_graph()
+tf.autograph.experimental.do_not_convert()
+
 def get_data_file():
-    data = np.load('../../../datasets/test_datasets.npy')
+    data = np.load(os.path.join(config.base_dir, 'datasets', 'test_datasets.npy'))
+    # test 50개만 
+    # img_paths = data[:config.do_sampling, :1]
+    # captions = data[:config.do_sampling, 2:]
     img_paths = data[:50, :1]
     captions = data[:50, 2:]
     train_images = np.squeeze(img_paths, axis=1)
-    train_images = ['../../../datasets/images/' + img for img in train_images]
+    train_images = [os.path.join(config.base_dir, 'datasets', 'images', f'{img}') for img in train_images]
     train_captions = np.squeeze(captions, axis=1)
     train_captions = ['<start>' + cap + ' <end>' for cap in train_captions]
     return train_images, train_captions
@@ -37,14 +45,13 @@ def load_image(image_path):
     img = tf.keras.applications.inception_v3.preprocess_input(img)
     return img, image_path
 
-
+# file 불러오기
 img_name_vector, train_captions = get_data_file()
 
-image_model = tf.keras.applications.InceptionV3(include_top=False,
-                                                weights='imagenet')
+# Inception V3 모델링
+image_model = tf.keras.applications.InceptionV3(include_top=False,weights='imagenet')
 new_input = image_model.input
 hidden_layer = image_model.layers[-1].output
-
 image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
 
 
@@ -52,32 +59,15 @@ image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
 encode_train = sorted(set(img_name_vector))
 
 
-
 image_dataset = tf.data.Dataset.from_tensor_slices(encode_train)
-# print(image_dataset)
-# <TensorSliceDataset shapes: (), types: tf.string>
-# print(image_dataset.enumerate())
-# <DatasetV1Adapter shapes: ((), ()), types: (tf.int64, tf.string)>
-
 image_dataset = image_dataset.map(
   load_image, num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(16)
 
-print(image_dataset)
-# <BatchDataset shapes: ((None, 299, 299, 3), (None,)), types: (tf.float32, tf.string)>
 for img, path in tqdm(image_dataset):
     batch_features = image_features_extract_model(img)
 
     batch_features = tf.reshape(batch_features,
                               (batch_features.shape[0], -1, batch_features.shape[3]))
-    # print()
-    # print('=======batch_features=====================')
-    # print(batch_features)
-    # print('for/for')
     for bf, p in zip(batch_features, path):
-        # print()
-        # print('p', p)
         path_of_feature = p.numpy().decode("utf-8")
-        image_name = path_of_feature.split('/')[-1].split('.')[0]
-        # print(path_of_feature)
-        # print(bf.numpy())
-        np.save(f'C:\\Users\\multicampus\\ai_sub2\\datasets\\features\\{image_name}.npy', bf.numpy())
+        np.save(os.path.join(config.base_dir, 'datasets','features',os.path.basename(path_of_feature).replace('jpg', 'npy')), bf.numpy())
