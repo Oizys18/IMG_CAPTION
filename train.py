d4517213a392c5ca757a43617f39a414c6ccfb58
이수진 <soojin4964@gmail.com>
2020-04-08 06:14:19 +0000

    Feature/encoder
    Former-commit-id: 956269ab220cf1110bd6c8756be603c7bbab3a96

--- train.py
+++ train.py
@@ -4,16 +4,18 @@ from utils import utils
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import os
+import numpy as np
+from models.encoder import CNN_Encoder
 
+tf.autograph.experimental.do_not_convert()
+tf.compat.v1.reset_default_graph()
 # config 저장
 utils.save_config()
-
-# 나중에 merge 이후로 config 에서 가져오기
-PATH = os.path.abspath('.') + '/datasets/'
+BASE_DIR = os.path.join(config.base_dir, 'datasets')
 
 # 전체 데이터셋을 분리해 저장하기
-train_datasets_path = PATH + 'train_datasets.npy'
-test_datasets_path = PATH + 'test_datasets.npy'
+train_datasets_path = os.path.join(BASE_DIR, 'train_datasets.npy')
+test_datasets_path = os.path.join(BASE_DIR, 'test_datasets.npy')
 if not os.path.exists(train_datasets_path):
     # 이미지 경로 및 캡션 불러오기
     dataset = preprocess.get_path_caption(config.caption_file_path)
@@ -23,37 +25,65 @@ else:
     print('저장 된 train_datasets 과 test_datasets 을 사용합니다.')
 
 # tokenizer 만들기
-tokenizer_path = PATH + 'tokenizer.pkl'
+tokenizer_path = os.path.join(BASE_DIR,'tokenizer.pkl')
 if not os.path.exists(tokenizer_path):
     preprocess.save_tokenizer(train_datasets_path)
     print('새로운 Tokenizer 를 저장합니다.')
 else:
     print('기존의 Tokenizer 를 사용합니다.')
 
-# 전처리 1 image_path 와 captions 데이터셋 불러오기
-dataset_path = train_datasets_path if config.do_what == 'train' else test_datasets_path
-img_name_vector, train_captions = preprocess.get_data_file(PATH, dataset_path)
 
-# 전처리 2 captions 의 text 를 토큰화
-cap_vector = preprocess.change_text_to_token(train_captions)
+# 이미지 특징 벡터 불러오기
+# Load the numpy files
+def map_func(img_name, cap):
+    feature_name = os.path.basename(img_name).decode('utf-8').replace('jpg', 'npy')
+    img_tensor = np.load((os.path.join(BASE_DIR, 'features', feature_name)))
+    return img_tensor, cap
 
-# 전처리 3 train 과 val 분리
-img_name_train, img_name_val, cap_train, cap_val = train_test_split(img_name_vector,
-                                                                    cap_vector,
-                                                                    test_size=0.2,
-                                                                    random_state=0)  # TODO config
-
-# 전처리 4 image 불러와서 정규화 하고 tf.data.datasets 으로 return
-# TODO 정규화 함수 추가 해야 합니다.
-# TODO 이 dataset 으로 수진이 pre_trained 모델 사용할 것
-image_dataset = preprocess.get_image_datasets(img_name_vector)
-
-# 전처리 5 pre_trained 모델을 통해서 특징 npy 파일로 저장하기
-
-
-# 전처리 6 Req 3-1 이미지 데이터 및 토큰화 된 캡션 쌍 리턴하기 및 Shuffle, batch
-# dataset = preprocess.get_tf_dataset(img_name_train, cap_train)
-# dataset = dataset.shuffle(config.buffer_size).batch(config.batch_size)  # TODO config
-# dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
 # 전처리 7 Req 3-2
+
+
+# hyperparameter
+embedding_dim = 256
+BUFFER_SIZE = 48
+BATCH_SIZE = 16
+
+
+
+################################################ 실행 여기서 부터
+# file load 
+img_name_vector, train_captions = preprocess.get_data_file()
+cap_vector = preprocess.change_text_to_token(train_captions)
+img_name_train, img_name_val, cap_train, cap_val = train_test_split(img_name_vector,
+                                                                    cap_vector,
+                                                                    test_size=config.test_size,
+                                                                    random_state=config.random_state)  
+
+# 학습을 위한 데이터셋 설정 (tf.data dataset)
+tf.compat.v1.enable_eager_execution()
+dataset = tf.data.Dataset.from_tensor_slices((img_name_train, cap_train))
+dataset = dataset.map(lambda item1, item2: tf.numpy_function(
+          map_func, [item1, item2], [tf.float32, tf.int32]),
+          num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+
+
+# Shuffle and batch
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+# for x, y in dataset:
+#     print(x, y)
+
+
+encoder = CNN_Encoder(embedding_dim)
+
+########### train
+for (batch, (img_tensor, target)) in enumerate(dataset):
+    # print(np.shape(img_tensor)) 
+    # (16, 64, 2048)
+    features = encoder(img_tensor)
+    # print(np.shape(features))
+    # (16, 64, 256)
+
