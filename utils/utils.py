5f2076894188c0609a778b6015ff9f0b29d03460
조현동 <jay.hyundong@gmail.com>
2020-04-10 00:56:28 +0900

    Mod save_config items
    Former-commit-id: 732ae77abb38700f84e2a4cca9acd669757acbe1

--- utils/utils.py
+++ utils/utils.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import numpy as np
 
 
-def save_config(base_dir): # TODO
+def save_config(base_dir):
 	config_path = os.path.join(base_dir, 'config.csv')
 	with open(config_path, 'a', newline='') as f:
 		w = csv.writer(f, delimiter='|', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -13,12 +13,15 @@ def save_config(base_dir): # TODO
 		date = datetime.now()
 		caption_file_path = config.caption_file_path
 		test_size = config.test_size
-		random_state = config.random_state
-		do_what = config.do_what
-		do_sampling = 1 if config.do_sampling else 0
 		sampling_size = config.do_sampling
+		random_state = config.random_state
+		val_size = config.val_size
+		buffer_size = config.buffer_size
+		batch_size = config.batch_size
+		epochs = config.epochs
 
-		w.writerow([date, caption_file_path, test_size, random_state, do_what, do_sampling, sampling_size])
+		w.writerow([date, caption_file_path, test_size, sampling_size, random_state,
+					val_size, buffer_size, batch_size, epochs])
 
 
 def map_func(img_name, cap):
