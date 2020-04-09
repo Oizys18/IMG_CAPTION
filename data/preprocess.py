f8596fbda499235e8f236306d66e77938cf6477b
양찬우 <oizys18@gmail.com>
2020-04-09 19:20:46 +0900

    Add img visualize
    Former-commit-id: 5355efbd600cd3277a1646b7551738acb61a07dd

--- data/preprocess.py
+++ data/preprocess.py
@@ -17,13 +17,20 @@ def get_path_caption(caption_file_path):
     return np.loadtxt(caption_file_path, delimiter='|', skiprows=1, dtype=np.str)
 
 
-def dataset_split_save(data, BASE_DIR, test_size=0.3):  # TODO config
+def dataset_split_save(data, test_size=0.3):  # TODO config
     train_dataset, val_dataset = train_test_split(data,
                                                   test_size=test_size,
                                                   shuffle=False)
 
-    np.save(os.path.join(BASE_DIR, 'train_datasets.npy'), train_dataset)
-    np.save(os.path.join(BASE_DIR, 'test_datasets.npy'), val_dataset)
+    np.savetxt(
+        './datasets/train_datasets.csv', train_dataset, fmt='%s', delimiter='|'
+    )
+    np.save('./datasets/train_datasets.npy', train_dataset)
+    np.savetxt(
+        './datasets/test_datasets.csv', val_dataset, fmt='%s', delimiter='|'
+    )
+    np.save('./datasets/test_datasets.npy', val_dataset)
+    return './datasets/train_datasets.npy', './datasets/test_datasets.npy'
 
 
 def get_data_file():
@@ -36,9 +43,6 @@ def get_data_file():
         n_of_sample = int(total_len * config.do_sampling)
         img_paths = data[:n_of_sample, :1]
         captions = data[:n_of_sample, 2:]
-    else:
-        img_paths = data[:, :1]
-        captions = data[:, 2:]
     train_images = np.squeeze(img_paths, axis=1)
     train_captions = np.squeeze(captions, axis=1)
     train_captions = ['<start>' + cap + ' <end>' for cap in train_captions]
@@ -47,41 +51,42 @@ def get_data_file():
     return train_images, train_captions
 
 
-def save_tokenizer(caption_num_words=5000):
-    tokenizer_path = os.path.join(BASE_DIR,'tokenizer.pkl')
-    if not os.path.exists(tokenizer_path):     
-        data = np.load(data_path)
-        captions = data[:, 2:]
-    
-        captions = np.squeeze(captions, axis=1)
-        captions = ['<start>' + cap + ' <end>' for cap in captions]
-    
-        tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=caption_num_words + 1,
-                                                          oov_token='<unk>',
-                                                          lower=True,
-                                                          split=' ',
-                                                          filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
-    
-        tokenizer.fit_on_texts(captions)
-        tokenizer.word_index['<pad>'] = 0
-        tokenizer.index_word[0] = '<pad>'
-        
-        with open('./datasets/tokenizer.pkl', 'wb') as f:
-            pickle.dump(tokenizer, f, protocol=pickle.HIGHEST_PROTOCOL)
-    else:
-        with open(tokenizer_path, 'rb') as f:
-            tokenizer = pickle.load(f)
-        
-    return tokenizer
+def save_tokenizer(data_path, caption_num_words=5000):
+    data = np.load(data_path)
+    captions = data[:, 2:]
 
+    captions = np.squeeze(captions, axis=1)
+    captions = ['<start>' + cap + ' <end>' for cap in captions]
 
-def change_text_to_token(train_captions, tokenizer_path):
-    with open(tokenizer_path, 'rb') as f:
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=caption_num_words + 1,
+                                                      oov_token='<unk>',
+                                                      lower=True,
+                                                      split=' ',
+                                                      filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
+
+    tokenizer.fit_on_texts(captions)
+    tokenizer.word_index['<pad>'] = 0
+    tokenizer.index_word[0] = '<pad>'
+
+    with open('./datasets/tokenizer.pkl', 'wb') as f:
+        pickle.dump(tokenizer, f, protocol=pickle.HIGHEST_PROTOCOL)
+        
+def calc_max_length(tensor):
+    return max(len(t) for t in tensor)
+
+def change_text_to_token(train_captions):
+    with open('./datasets/tokenizer.pkl', 'rb') as f:
         tokenizer = pickle.load(f)
     train_seqs = tokenizer.texts_to_sequences(train_captions)
+    max_length = calc_max_length(train_seqs)
     cap_vector = tf.keras.preprocessing.sequence.pad_sequences(
         train_seqs, padding='post')
-    return cap_vector
+    print()
+    print('전처리 step 2')
+    print('캡션 텍스트 토큰화 ex) ')
+    print('1: ', cap_vector[:1])
+    print('2: ', cap_vector[1:2])
+    return cap_vector, max_length
 
 
 def load_image(image_path):
@@ -92,24 +97,23 @@ def load_image(image_path):
     return img, image_path
 
 
-# def get_image_datasets(img_name_vector):
-#     encode_train = sorted(set(img_name_vector))
-#     image_dataset = list(map(load_image, encode_train))
-#     # image_dataset = tf.data.Dataset.from_tensor_slices(encode_train)
-#     # image_dataset = image_dataset.map(
-#     #     load_image, num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(16)  # TODO batch
-#     return image_dataset
+def get_image_datasets(img_name_vector):
+    encode_train = sorted(set(img_name_vector))
+    image_dataset = list(map(load_image, encode_train))
+    # image_dataset = tf.data.Dataset.from_tensor_slices(encode_train)
+    # image_dataset = image_dataset.map(
+    #     load_image, num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(16)  # TODO batch
+    return image_dataset
 
 
 def map_func(img_name, cap):
-    feature_name = os.path.basename(img_name).decode('utf-8').replace('jpg', 'npy')
-    img_tensor = np.load((os.path.join(BASE_DIR, 'features', feature_name)))
+    img_tensor = np.load(img_name.decode('utf-8') + '.npy')
     return img_tensor, cap
 
 
-# def get_tf_dataset(img_name_train, cap_train):
-#     dataset = tf.data.Dataset.from_tensor_slices((img_name_train, cap_train))
-#     dataset = dataset.map(lambda item1, item2: tf.numpy_function(
-#         map_func, [item1, item2], [tf.float32, tf.int32]),
-#         num_parallel_calls=tf.data.experimental.AUTOTUNE)
-#     return dataset
+def get_tf_dataset(img_name_train, cap_train):
+    dataset = tf.data.Dataset.from_tensor_slices((img_name_train, cap_train))
+    dataset = dataset.map(lambda item1, item2: tf.numpy_function(
+        map_func, [item1, item2], [tf.float32, tf.int32]),
+        num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    return dataset
--- train.py
+++ train.py
@@ -8,12 +8,9 @@ import os
 import numpy as np
 from models.encoder import CNN_Encoder
 from models.decoder import RNN_Decoder
-import time
-import matplotlib.pyplot as plt
 
 tf.autograph.experimental.do_not_convert()
 tf.compat.v1.reset_default_graph()
-# tf.autograph.set_verbosity(3, True)
 # config 저장
 utils.save_config()
 BASE_DIR = os.path.join(config.base_dir, 'datasets')
@@ -24,74 +21,37 @@ test_datasets_path = os.path.join(BASE_DIR, 'test_datasets.npy')
 if not os.path.exists(train_datasets_path):
     # 이미지 경로 및 캡션 불러오기
     dataset = preprocess.get_path_caption(config.caption_file_path)
-    preprocess.dataset_split_save(dataset, BASE_DIR, config.test_size)
+    preprocess.dataset_split_save(dataset, config.test_size)
     print('dataset 을 train_datasets 과 test_datasets 으로 나눕니다.')
 else:
     print('저장 된 train_datasets 과 test_datasets 을 사용합니다.')
 
-# tokenizer 불러오기
-tokenizer = preprocess.save_tokenizer(train_datasets_path)
-tokenizer_path = os.path.join(BASE_DIR, 'tokenizer.pkl')
+# tokenizer 만들기
+tokenizer_path = os.path.join(BASE_DIR,'tokenizer.pkl')
+if not os.path.exists(tokenizer_path):
+    preprocess.save_tokenizer(train_datasets_path)
+    print('새로운 Tokenizer 를 저장합니다.')
+else:
+    print('기존의 Tokenizer 를 사용합니다.')
 
 
-def loss_function(real, pred):
-  mask = tf.math.logical_not(tf.math.equal(real, 0))
-  loss_ = loss_object(real, pred)
 
-  mask = tf.cast(mask, dtype=loss_.dtype)
-  loss_ *= mask
-
-  return tf.reduce_mean(loss_)
+# feature extraction 이미지 특징 생성하기
+feature_extraction()
 
 
-@tf.function
-def train_step(img_tensor, target):
-  loss = 0
+# 이미지 특징 벡터 불러오기
+# Load the numpy files
+def map_func(img_name, cap):
+    feature_name = os.path.basename(img_name).decode('utf-8').replace('jpg', 'npy')
+    img_tensor = np.load((os.path.join(BASE_DIR, 'features', feature_name)))
+    return img_tensor, cap
 
-  # initializing the hidden state for each batch
-  # because the captions are not related from image to image
-  hidden = decoder.reset_state(batch_size=target.shape[0])
 
-  dec_input = tf.expand_dims([tokenizer.word_index['<start>']] * target.shape[0], 1)
 
-  with tf.GradientTape() as tape:
-    # print(np.shape(img_tensor)) 
-    # (16, 64, 2048)
-      features = encoder(img_tensor)
-    # print(np.shape(features))
-    # (16, 64, 256)
-      for i in range(1, target.shape[1]):
-          # passing the features through the decoder
-          predictions, hidden, _ = decoder(dec_input, features, hidden)
-
-          loss += loss_function(target[:, i], predictions)
-
-          # using teacher forcing
-          dec_input = tf.expand_dims(target[:, i], 1)
-
-  total_loss = (loss / int(target.shape[1]))
-
-  trainable_variables = encoder.trainable_variables + decoder.trainable_variables
-
-  gradients = tape.gradient(loss, trainable_variables)
-
-  optimizer.apply_gradients(zip(gradients, trainable_variables))
-
-  return loss, total_loss
 
 # 전처리 7 Req 3-2
 
-
-
-
-################################################ 실행 여기서 부터
-# file load 
-img_name_vector, train_captions = preprocess.get_data_file()
-cap_vector = preprocess.change_text_to_token(train_captions, tokenizer_path)
-img_name_train, img_name_val, cap_train, cap_val = train_test_split(img_name_vector,
-                                                                    cap_vector,
-                                                                    test_size=config.test_size,
-                                                                    random_state=config.random_state)  
 
 # hyperparameter
 embedding_dim = 256
@@ -99,18 +59,25 @@ BUFFER_SIZE = 48
 BATCH_SIZE = 16
 units = 512
 vocab_size = 5000
-EPOCHS = 6
-num_steps = len(img_name_train)
 
+
+################################################ 실행 여기서 부터
+# file load 
+img_name_vector, train_captions = preprocess.get_data_file()
+cap_vector,max_length = preprocess.change_text_to_token(train_captions)
+img_name_train, img_name_val, cap_train, cap_val = train_test_split(img_name_vector,
+                                                                    cap_vector,
+                                                                    test_size=config.test_size,
+                                                                    random_state=config.random_state)  
 
 # 학습을 위한 데이터셋 설정 (tf.data dataset)
 tf.compat.v1.enable_eager_execution()
 dataset = tf.data.Dataset.from_tensor_slices((img_name_train, cap_train))
-
-# 이미지 특징 벡터 불러오기
 dataset = dataset.map(lambda item1, item2: tf.numpy_function(
-          preprocess.map_func, [item1, item2], [tf.float32, tf.int32]),
+          map_func, [item1, item2], [tf.float32, tf.int32]),
           num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+
 
 # Shuffle and batch
 dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
@@ -119,52 +86,15 @@ dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 # for x, y in dataset:
 #     print(x, y)
 
-########### CNN RNN 모델
+
 encoder = CNN_Encoder(embedding_dim)
 decoder = RNN_Decoder(embedding_dim, units, vocab_size)
 
-
-########### optimizer & loss object 
-optimizer = tf.keras.optimizers.Adam()
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
-    from_logits=True, reduction='none')
-
-############# 체크포인트 매니저 생성
-checkpoint_path = os.path.join(config.base_dir, 'checkpoints')
-ckpt = tf.train.Checkpoint(encoder=encoder,
-                           decoder=decoder,
-                           optimizer = optimizer)
-ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=3)
-
-
 ########### train
-start_epoch = 0
-if ckpt_manager.latest_checkpoint:
-  start_epoch = int(ckpt_manager.latest_checkpoint.split('-')[-1])
-  print("마지막으로 저장된 epoch {} 부터 시작합니다.".format(start_epoch + 1))
-  ckpt.restore(ckpt_manager.latest_checkpoint)
+for (batch, (img_tensor, target)) in enumerate(dataset):
+    # print(np.shape(img_tensor)) 
+    # (16, 64, 2048)
+    features = encoder(img_tensor)
+    # print(np.shape(features))
+    # (16, 64, 256)
 
-loss_plot = []
-for epoch in range(start_epoch, EPOCHS):
-    start = time.time()
-    total_loss = 0
-
-    for (batch, (img_tensor, target)) in enumerate(dataset):
-        batch_loss, t_loss = train_step(img_tensor, target)
-        total_loss += t_loss
-
-        if batch % 100 == 0:
-            print ('Epoch {} Batch {} Loss {:.4f}'.format(
-              epoch + 1, batch, batch_loss.numpy() / int(target.shape[1])))
-    # storing the epoch end loss value to plot later
-    loss_plot.append(total_loss / num_steps)
-
-    # checkpoint
-    if epoch % 5 == 0:
-        ckpt_manager.save()
-        ckpt_path = ckpt_manager.save()
-        print('Epoch {} 저장 : {}'.format(epoch+1, ckpt_path))
-
-    print ('Epoch {} Loss {:.6f}'.format(epoch + 1,
-                                         total_loss/num_steps))
-    print ('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
